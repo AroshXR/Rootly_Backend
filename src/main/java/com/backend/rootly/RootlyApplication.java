b354ba2e1fef2393c37b1b30a7de47dba8d376c6
aroshana_a <aroshana_a@epiclanka.net>
2026-08-01 15:47:50 +0530

    Initial commit

---
+++ src/main/java/com/backend/rootly/RootlyApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rootly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/rootly/RootlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rootly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
