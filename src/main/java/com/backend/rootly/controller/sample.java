9f605c7cbb24124e707d96209d5056f405ce7acf
Dilhara A.A.D <dasun5967@gmail.com>
2026-08-22 01:06:40 +0530

    check PMD

--- src/main/java/com/backend/rootly/controller/sample.java
+++ src/main/java/com/backend/rootly/controller/sample.java
@@ -1,10 +1,12 @@
 package com.backend.rootly.controller;
 
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class sample {
     public void testMethod() {
         int unusedVariable = 10;
-        System.out.println(unusedVariable);
+        log.debug(unusedVariable);
     }
 
 }
