04ce8ca74a5df6e094c22bfff93405f5f380833b
Dilhara A.A.D <dasun5967@gmail.com>
2026-08-22 00:51:51 +0530

    check PMD

--- src/main/java/com/backend/rootly/controller/sample.java
+++ src/main/java/com/backend/rootly/controller/sample.java
@@ -1,5 +1,10 @@
 package com.backend.rootly.controller;
 
+
 public class sample {
+    public void testMethod() {
+        int unusedVariable = 10;
+        System.out.println(unusedVariable);
+    }
 
 }
